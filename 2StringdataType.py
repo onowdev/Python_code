6328843162035fd934b672ac1f54dfb61403001e
Agus Sugiono <onowdev@gmail.com>
2023-01-12 18:06:11 +0700

    add Str

---
+++ 2StringdataType.py
@@ -0,0 +1,11 @@
+'''
+String Data Type
+String are identified as a contiguous set of characters represented in the quotation marks. Python allows for either
+pairs of single or double quotes. Strings are immutable sequence data type, i.e each time one makes any changes
+to a string, completely new string object is created.
+
+'''
+a_str = 'Hello Python'
+print(a_str)
+print(a_str[1]) # output is e
+print(a_str[3:6])

4aec5b0668d30644bf7f64894519357631cdb09e
Agus Sugiono <onowdev@gmail.com>
2023-01-13 15:22:25 +0700

    contoh list

---
+++ 6listDataType.py
@@ -0,0 +1,13 @@
+'''
+A list contains items separated by commas and enclosed within square brackets [].
+lists are almost similar to arrays
+in C. One difference is that all the items belonging to a list can be of different data type
+'''
+
+contoh_list = [123, 'abcd', 19.9, 'A']
+contoh_list2 = ['hallo', 'dunia']
+
+print(contoh_list)
+print(contoh_list[1:3])
+print(contoh_list2 * 4)
+print(contoh_list + contoh_list2)

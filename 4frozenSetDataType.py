216ead4bcda947efeec665188af6225ca3cef1e7
Agus Sugiono <onowdev@gmail.com>
2023-01-13 15:07:15 +0700

    frozenset

---
+++ 4frozenSetDataType.py
@@ -0,0 +1,9 @@
+# Frozen Sets - They are immutable and new elements cannot added after its defined.
+
+b = frozenset('Buah-Buahan')
+
+print(b)
+
+kota = frozenset(['Semarang', 'Milan', 'Kudus', 'Jakarta', 'Palembang'])
+
+print(kota)

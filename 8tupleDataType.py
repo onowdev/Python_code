7bdcfe70e6b3b2dbb43bfb9f4dc483faabeb68e4
Agus Sugiono <onowdev@gmail.com>
2023-01-13 15:32:48 +0700

    tupleDataSet

---
+++ 8tupleDataType.py
@@ -0,0 +1,14 @@
+# Tuple Data Type
+'''
+Lists are enclosed in brackets [ ] and their elements and size can be changed, while tuples are enclosed in
+parentheses ( ) and cannot be updated. Tuples are immutable.
+'''
+
+tuple_a = (123,'hello')
+tuple_b = ('world')
+
+print(tuple_a)
+print(tuple_b)
+print(tuple_a[1])
+print(tuple_b[0])
+# print(tuple_a +  tuple_b)

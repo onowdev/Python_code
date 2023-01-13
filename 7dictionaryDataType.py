8e88e8c6ade1757e43a5414172ca933e411fff4f
Agus Sugiono <onowdev@gmail.com>
2023-01-13 15:27:28 +0700

    type Dictionary

---
+++ 7dictionaryDataType.py
@@ -0,0 +1,13 @@
+# dictionary Data type
+'''
+Dictionary consists of key-value pairs. 
+It is enclosed by curly braces {} and values can be assigned and accessed
+using square brackets[]
+'''
+
+typeDictionary = {'name':'MightyEagle' , 'age':20}
+
+print(typeDictionary)
+print(typeDictionary['name'])
+print(typeDictionary.values())
+print(typeDictionary.keys())

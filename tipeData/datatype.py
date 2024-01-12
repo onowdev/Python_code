7a5eb24447a8dc2f18c2a4cefcd72f119e6d7d62
onowdev <onowdev@gmail.com>
2024-01-12 10:21:58 +0700

    kuis data type

---
+++ tipeData/datatype.py
@@ -0,0 +1,24 @@
+"""
+TODO:
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string, 
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+firstName = "Agus"
+lastName = "Sugiono"
+age = 34
+isMarried = True
+
+print("Biodata Anda : \n")
+
+print(firstName)
+print(lastName)
+print(age)
+print(isMarried)

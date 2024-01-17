b9e63cb0e1a734f476995527884adb7eefc02133
onowdev <onowdev@gmail.com>
2024-01-17 10:02:39 +0700

    jenis data type

---
+++ tipeData/kindOfTypeData.py
@@ -0,0 +1,29 @@
+a = "Hello World"
+b = 20
+c = 90.99
+d = 9j
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = range(6)
+h = {"name" : "Agus", "age" : 34}
+i = frozenset({"apple", "banana", "cherry"})
+j = True
+k = b"hello"
+l = bytearray(8)
+m = memoryview(bytes(7))
+n = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))

4f8759cbd06a5150b214a9bb04e1bb27a52f7647
onowdev <onowdev@gmail.com>
2024-01-19 13:46:56 +0700

    add reverse true

--- shorting/sort.py
+++ shorting/sort.py
@@ -1,5 +1,9 @@
 motor = ['yamaha', 'honda', 'suzuki', 'kawasaki', 'vespa', 'benelli', 'moto guzzi']
 
+mobil = ['bmw', 'daihatsu', 'mercedez-benz', 'ferarri', 'bentley', 'aston martin', 'toyota']
+
 motor.sort()
+mobil.sort(reverse=True)
 
 print(motor)
+print(mobil)

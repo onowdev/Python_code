b457189f2024cf70b5ee6c8f8e136110cb62ac24
onowdev <onowdev@gmail.com>
2024-01-26 15:05:45 +0700

    tebak nomor

--- basic/implementation/guessingNumberGame.py
+++ basic/implementation/guessingNumberGame.py
@@ -3,7 +3,7 @@ import random
 secret_number = random.randint(1, 100)
 
 while True:
-    guess = int(input("Guess the number between 1 and 100 \n"))
+    guess = int(input("Guess the number between 1 and 100 \n90"))
     
     if guess == secret_number:
         print("Congratulations! You guessed the number!")

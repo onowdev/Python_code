f4d300f4f1a2903688729f1193cec4b67fc3420c
onowdev <onowdev@gmail.com>
2024-02-02 15:24:39 +0700

    elif fungsi

---
+++ IfElse/KeyWorldElif.py
@@ -0,0 +1,9 @@
+str_input = input('Enter your grade: ')
+grade = int(str_input)
+
+if grade == 100:
+    print("perfect")
+elif grade >= 85:
+    print("awesome")
+elif grade >= 65:
+    print("passed the exam")

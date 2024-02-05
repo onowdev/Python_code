2e7017a8b6a059478922e4cf2cc1130ac2e6058c
onowdev <onowdev@gmail.com>
2024-02-05 11:22:48 +0700

    perulangan Dasar For

---
+++ perulangan/perulanganFor.py
@@ -0,0 +1,14 @@
+"""
+Perulangan atau loop merupakan teknik untuk mengulang-ulang
+eksekusi suatu blok kode, atau mengiterasi elemen milik tipe data kolektif (contohnya: list).
+
+
+Perulangan di Python bisa dibuat menggunakan kombinasi keyword for dan fungsi range().
+
+Keyword for adalah keyword untuk perulangan, dalam penerapannya diikuti dengan keyword in.
+Fungsi range() digunakan untuk membuat object range, yang umumnya dipakai sebagai kontrol perulangan.
+
+"""
+
+for i in range(5):
+    print("index", i)

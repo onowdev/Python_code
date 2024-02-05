00f295210c9eb998880c0ebd970a3b2a3d3a7999
onowdev <onowdev@gmail.com>
2024-02-05 11:40:23 +0700

    add index statement

--- perulangan/perulanganFor.py
+++ perulangan/perulanganFor.py
@@ -12,3 +12,14 @@ Fungsi range() digunakan untuk membuat object range, yang umumnya dipakai sebaga
 
 for i in range(5):
     print("index", i)
+
+
+"""
+Penjelasan:
+
+Statement print("index:", i) muncul 5 kali, karena perulangan dilakukan dengan kontrol range(5) dimana statement tersebut menghasilkan object range dengan isi deret angka sejumlah 5 dimulai dari angka 0 hingga 4.
+
+Statement for i in range(5): adalah contoh penulisan perulangan menggunakan for dan range(). Variabel i berisi nilai counter setiap iterasi, yang pada konteks ini adalah angka 0 hingga 4.
+
+Statement print("index:", i) wajib ditulis menjorok ke kanan karena merupakan isi dari blok perulangan for i in range(5):...
+"""
